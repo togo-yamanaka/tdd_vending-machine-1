b91892074e9771e68d57b386fc2e7b18611a7284
togo-yamanaka <togo.yamanaka@ariseanalytics.com>
2020-08-20 12:56:57 +0900

    second yamanaka commit

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,45 +1,53 @@
 import pytest
 
-from vending_machine.vending_machine import vending_machine
+from vending_machine.vending_machine import VendingMachine, Coin
 
 
 class TestVendingMachine:
-    @pytest.mark.parametrize(
-        "input, total_amount, return_money",
-        [
-            (1, 0, 1),
-            (5, 0, 5),
-            (10, 10, None),
-            (50, 50, None),
-            (100, 100, None),
-            (500, 500, None),
-            (1000, 1000, None),
-        ],
-    )
-    def test_insert(self, input, total_amount, return_money) -> None:
-        machine = vending_machine()
-        change = machine.insert(input)
+    # @pytest.mark.parametrize(
+    #     "input, total_amount, return_money",
+    #     [
+    #         (1, 0, 1),
+    #         (5, 0, 5),
+    #         (10, 10, None),
+    #         (50, 50, None),
+    #         (100, 100, None),
+    #         (500, 500, None),
+    #         (1000, 1000, None),
+    #     ],
+    # )
+    def test_insert(self):  # , input, total_amount, return_money) -> None:
+        machine = VendingMachine()
+        coin = Coin(50)
 
-        assert machine.get_total_amount() == total_amount
-        assert change == return_money
+        machine.insert(coin)
+
+        assert machine.get_total_amount() == coin.amount
 
     def test_get_total_amount(self) -> None:
-        machine = vending_machine()
-        machine.insert(10)
-        machine.insert(50)
-        machine.insert(100)
-        machine.insert(500)
-        machine.insert(1000)
+        machine = VendingMachine()
+        machine.insert(Coin(10))
+        machine.insert(Coin(50))
+        machine.insert(Coin(100))
+        machine.insert(Coin(500))
+        machine.insert(Coin(1000))
 
         assert machine.get_total_amount() == 1660
 
     def test_refund(self) -> None:
-        machine = vending_machine()
-        machine.insert(10)
-        machine.insert(50)
-        machine.insert(100)
-        machine.insert(500)
-        machine.insert(1000)
+        machine = VendingMachine()
+        machine.insert(Coin(10))
+        machine.insert(Coin(50))
+        machine.insert(Coin(100))
+        machine.insert(Coin(500))
+        machine.insert(Coin(1000))
 
         assert machine.refund() == 1660
         assert machine.get_total_amount() == 0
+
+    def test_invalid_coin_generate(self):
+
+        try:
+            Coin(320)
+        except Exception as e:
+            assert e.message == "正常なコインではありません"
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,18 +1,29 @@
-class vending_machine:
-    def __init__(self) -> None:
-        self.money = 0
+class Coin(object):
+    def __init__(self, amount) -> None:
+        self.available_amount = [10, 50, 100, 500, 1000]
 
-    def insert(self, add_money) -> None:
-        avalable_money = [10, 50, 100, 500, 1000]
-        if add_money in avalable_money:
-            self.money += add_money
+        if amount in self.available_amount:
+            self.amount = amount
         else:
-            return add_money
+            raise Exception("正常なコインではありません")
+
+
+class VendingMachine:
+    def __init__(self) -> None:
+        self.coin_list = []
+
+    def insert(self, coin: Coin) -> None:
+        self.coin_list.append(coin)
 
     def get_total_amount(self) -> int:
-        return self.money
+        if self.coin_list:
+            result = sum([coin.amount for coin in self.coin_list])
+        else:
+            result = 0
+        return result
 
     def refund(self) -> int:
-        change = self.money
-        self.money = 0
-        return change
+        result = self.get_total_amount()
+        self.coin_list = []
+        return result
+
