4acc3c8fd986e55dd86fd0fd36f26fe14b4cac84
togo-yamanaka <togo.yamanaka@ariseanalytics.com>
2021-01-04 16:42:37 +0900

    add main.py

---
+++ cc9/main.py
@@ -0,0 +1,26 @@
+"""
+main
+"""
+
+import sys
+
+from tokenizer import tokenize, TokenOperator
+from parser import Parser
+from code_generator import generate_code
+
+
+def main(argv: list) -> int:
+    """main関数"""
+    if len(argv) != 2:
+        raise ValueError("引数の個数が正しくありません")
+
+    print(argv)
+
+    operator = TokenOperator(tokenize(argv[1]))
+    parser = Parser(operator)
+    node = parser.run()
+    generate_code(node)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
